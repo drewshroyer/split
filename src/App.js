74ee3b779653750bdc936ae4281465cb17bc94b2
drewshroyer <drewshroyer@gmail.com>
2021-03-02 21:48:36 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <div className="website-container">
       <header className="App-header">
-
         {/* NAV BAR */}
         {/* USERS
         {/* EXPENSES */}
